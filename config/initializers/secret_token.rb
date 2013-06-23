5cb9bd2fc2802b8a81a686f07afa1a204af37237
Al Johri <al.johri@gmail.com>
2013-06-23 03:08:05 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EveryLastMorsel::Application.config.secret_token = '1228f45611cd6222e2d136f2aa3dd79438423d7f6579f29aca58116bf9a29474d05a4a905d5f1066b57483febe3d1b16d2a45a223d85e60ecf19df9931cd3a5e'
