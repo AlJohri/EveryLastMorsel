3410874af582f49328dd42f2e784b3e896f47571
Al Johri <al.johri@gmail.com>
2013-07-04 15:19:11 -0400

    Changed production mailer URL.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ EveryLastMorsel::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'dev-everylastmorsel.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
