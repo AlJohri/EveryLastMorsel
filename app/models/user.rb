dbe5bdaaaa4d239f9d6101260d83d79755edd256
Al Johri <al.johri@gmail.com>
2013-07-05 11:30:09 -0400

    Removed confirmable module from Devise.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter]
+         # :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
