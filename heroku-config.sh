4ce41b7436e0e4badf4d7bb8f7d2e9b958922dbf
Al Johri <al.johri@gmail.com>
2013-07-14 14:29:46 -0400

    Modified heroku-config shell script.

--- heroku-config.sh
+++ heroku-config.sh
@@ -2,5 +2,6 @@
 
 rake figaro:heroku\[everylastmorsel\]
 rake figaro:heroku\[dev-everylastmorsel\]
-#rake figaro:heroku\[alpha-everylastmorsel\]
+rake figaro:heroku\[alpha-everylastmorsel\]
 rake figaro:heroku\[beta-everylastmorsel\]
+rake figaro:heroku\[elm\]
