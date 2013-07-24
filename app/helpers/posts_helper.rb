5c6f9f136d202410032c7e76ae796502df0e498e
Al Johri <al.johri@gmail.com>
2013-07-24 16:48:37 -0400

    Add line spacing after paragraph for two line breaks or one line break.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,27 @@
 module PostsHelper
 
+	def simple_format2(text, html_options = {}, options = {})
+	  wrapper_tag = options.fetch(:wrapper_tag, :p)
+
+	  text = sanitize(text) if options.fetch(:sanitize, true)
+	  paragraphs = split_paragraphs2(text)
+
+	  if paragraphs.empty?
+	    content_tag(wrapper_tag, nil, html_options)
+	  else
+	    paragraphs.map { |paragraph|
+	      content_tag(wrapper_tag, paragraph, html_options, options[:sanitize])
+	    }.join("\n\n").html_safe
+	  end
+	end
+
+    def split_paragraphs2(text)
+      return [] if text.blank?
+
+      text.to_str.gsub(/\r\n?/, "\n").split(/\n+/).map! do |t|
+        t.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') || t
+      end
+    end
+
+
 end
