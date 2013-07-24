cccf0098ad554e9ae731d4ed90f7b591ef77ec02
Al Johri <al.johri@gmail.com>
2013-07-24 05:18:55 -0400

    Replaced logo with new beta logo. Changed Rails to use sessions instead of cookies because Facebook keeps overflowing the 4kb limit.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-EveryLastMorsel::Application.config.session_store :cookie_store, key: '_EveryLastMorsel_session'
+# EveryLastMorsel::Application.config.session_store :cookie_store, key: '_EveryLastMorsel_session'
+EveryLastMorsel::Application.config.session_store :active_record_store
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
