6a6231463a8754592b3ff48b80c216f0d37bf464
Al Johri <al.johri@gmail.com>
2013-08-10 10:30:08 -0400

    Removed Rails Admin due to bootstrap3 incompatibility. Began styling profile.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,19 +1,17 @@
 # RailsAdmin config file. Generated on August 01, 2013 23:37
 # See github.com/sferik/rails_admin for more informations
 
-RailsAdmin.config do |config|
+#RailsAdmin.config do |config|
 
   ################  Global configuration  ################
 
   # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = ['Every Last Morsel', 'Admin']
   # or for a more dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
-  # RailsAdmin may need a way to know who the current user is]
-  config.current_user_method { current_user } # auto-generated
-
-  config.authorize_with :cancan
+  ## config.main_app_name = ['Every Last Morsel', 'Admin']
+  ## config.current_user_method { current_user }
+  ## config.authorize_with :cancan
 
   # If you want to track changes on your models:
   # config.audit_with :history, 'User'
@@ -1022,4 +1020,4 @@ RailsAdmin.config do |config|
   #     # using `field` instead of `configure` will exclude all other fields and force the ordering
   # end
 
-end
+#end
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ EveryLastMorsel::Application.routes.draw do
   }
 
   ############## Rails Admin ##############
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  # mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   ####################################################################################
   # CONCERNS
