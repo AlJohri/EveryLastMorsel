d4b17166d0f58ad2ae75f00cf73e7e564d3a7129
Peter Jabbour <petejabbour1@gmail.com>
2013-10-02 13:51:45 -0500

    add crop_yields for sale

--- app/controllers/crop_yields_controller.rb
+++ app/controllers/crop_yields_controller.rb
@@ -1,4 +1,5 @@
 class CropYieldsController < InheritedResources::Base
+  before_filter :user_has_active_merchant_account, only: [:update]
 
   respond_to :html, :xml, :json
 
@@ -23,5 +24,13 @@ class CropYieldsController < InheritedResources::Base
       render "crops/partials/show"
     end
   end
+  
+  private
+  
+    def user_has_active_merchant_account
+      unless current_user.merchant_account && current_user.merchant_account.active?
+        redirect_to new_user_merchant_account_path(current_user), notice: "Please create a merchant account."
+      end
+    end
 
 end
--- app/models/crop_yield.rb
+++ app/models/crop_yield.rb
@@ -6,4 +6,8 @@ class CropYield < ActiveRecord::Base
   include PublicActivity::Model
   tracked owner: ->(controller, model) { controller && controller.current_user }  
   
+  
+  def for_sale?
+    self.quantity_for_sale > 0 ? true : false
+  end
 end
