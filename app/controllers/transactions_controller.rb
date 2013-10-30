412940c84f53076298e6778e17c59f8605e3c7e3
Peter Jabbour <petejabbour1@gmail.com>
2013-10-30 14:15:32 -0500

    transaction validations and form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,19 @@
 class TransactionsController < ApplicationController
-  
+    
   def new
     @transaction = Transaction.new
+    @crop_yield = CropYield.find_by_id(params[:crop_yield])
+  end
+  
+  def create
+    @transaction = Transaction.new(params[:transaction])
+    @crop_yield = CropYield.find_by_id(params[:transaction][:crop_yield_id])
+    
+    if @transaction.save
+      # error if purchasing too many
+      # change crop_yield quantity to subtract from transaction quantity
+    else
+      render 'new'
+    end
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :user_id, :amount, :crop_yield_id, :quantity
   
+  validates :amount, presence: true
+  validates :crop_yield_id, presence: true
+  validates :quantity, presence: true
+  
 end
